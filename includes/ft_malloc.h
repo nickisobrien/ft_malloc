39a287f9840549d27262a4cecea1552d558f9176
Nicholas O'brien <nobrien@e1z3r1p23.42.us.org>
2018-09-28 15:23:21 -0700

    init

--- includes/ft_malloc.h
+++ includes/ft_malloc.h
@@ -1,2 +1,7 @@
 #ifndef __FT_MALLOC
 #define __FT_MALLOC
+
+#include <stdio.h>
+#include <sys/mman.h>
+
+#endif
--- srcs/ft_malloc.c
+++ srcs/ft_malloc.c
@@ -1,1 +1,32 @@
+#include <ft_malloc.h>
 
+void free(void *ptr)
+{(void)ptr;
+
+}
+
+void *malloc(size_t size)
+{
+	void *addr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	if (addr == MAP_FAILED)
+	{
+		printf("FAILED\n");
+	}
+	return (addr);
+}
+
+void *realloc(void *ptr, size_t size)
+{(void)ptr;(void)size;
+	return (NULL);
+}
+
+int		main(void)
+{
+	char *str = (char *)malloc(sizeof(char) * 5);
+	for (int i = 0; i < 4; i++)
+	{
+		str[i] = 'a' + i;
+	}
+	str[4] = '\0';
+	printf("Test %s\n", str);
+}
