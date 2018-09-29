7ce90484fa33ad73482c696a1b48a3d371b333c4
Nicholas O'brien <nobrien@e1z3r1p23.42.us.org>
2018-09-29 11:56:52 -0700

    commit

--- includes/ft_malloc.h
+++ includes/ft_malloc.h
@@ -4,4 +4,15 @@
 #include <stdio.h>
 #include <sys/mman.h>
 
+typedef struct	s_node
+{
+	void *memory;
+	int size;
+}				t_node;
+
+typedef struct	s_lst
+{
+	t_node *head;
+}				t_lst;
+
 #endif
--- srcs/ft_malloc.c
+++ srcs/ft_malloc.c
@@ -1,23 +1,31 @@
 #include <ft_malloc.h>
 
-void free(void *ptr)
-{(void)ptr;
+void free(void *addr)
+{(void)addr;
 
-}
-
-void *malloc(size_t size)
-{
-	void *addr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-	if (addr == MAP_FAILED)
-	{
-		printf("FAILED\n");
-	}
-	return (addr);
+	munmap(addr, 5); // void*, size
 }
 
 void *realloc(void *ptr, size_t size)
 {(void)ptr;(void)size;
 	return (NULL);
+}
+
+// void	add_to_list(void *ptr, int size)
+// {
+// 	(void)ptr;(void)size;
+// }
+
+void *malloc(size_t size)
+{
+	void *addr = mmap(0, size,
+		PROT_READ | PROT_WRITE | PROT_EXEC,
+		MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	if (addr == MAP_FAILED)
+		return (NULL);
+	// add_to_list(addr, size);
+
+	return (addr);
 }
 
 int		main(void)
@@ -28,5 +36,7 @@ int		main(void)
 		str[i] = 'a' + i;
 	}
 	str[4] = '\0';
+	printf("Test %s\n", str);
+	free(str);
 	printf("Test %s\n", str);
 }
