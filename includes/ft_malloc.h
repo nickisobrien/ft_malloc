7d7b5e5d0627bf3c98a5692233a1179e0da23ecd
Nick <hoop.bbb@gmail.com>
2018-09-30 13:29:07 -0700

    getting close

--- includes/ft_malloc.h
+++ includes/ft_malloc.h
@@ -3,26 +3,45 @@
 
 #include <stdio.h>
 #include <sys/mman.h>
+#include <unistd.h>
 
 // typedef struct	s_node
 // {
-// 	void *ptr;
-// 	int size;
+// 	void *mem;
 // 	t_node *next;
 // }				t_node;
 
-// typedef struct	s_lst
-// {
-// 	t_node *node;
-// }				t_lst;
+typedef struct		s_global
+{
+	void *tiny;
+	void *small;
+	void *large;
+}					t_global;
 
-typedef struct {
+typedef struct		s_block_header
+{
 	size_t size;
 	char allocated;
-} block_header;
+}					block_header;
 
+
+#define CHUNK_SIZE (1 << 14)
+#define CHUNK_ALIGN(size) (((size)+(CHUNK_SIZE-1)) & ~(CHUNK_SIZE-1))
 // block overhead
 #define OVERHEAD sizeof(block_header)
+// get header from payload pointer (bp = block payload)
+#define HDRP(bp) ((char *)(bp) - sizeof(block_header))
+// p = block header
+#define GET_SIZE(p) ((block_header *)(p))->size
+// is allocated?
+#define GET_ALLOC(p) ((block_header *)(p))->allocated
+// get next block header (bp = block paylaod)
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))
+
+#define TINY 100
+#define SMALL 4096
+// large is anything > SMALL
 
 
+#define PAGE_SIZE getpagesize()
 #endif
--- srcs/ft_malloc.c
+++ srcs/ft_malloc.c
@@ -1,8 +1,14 @@
 #include <ft_malloc.h>
 
+t_global global;
+
 void free(void *addr)
 {(void)addr;
+	// if there is nothing in the block, munmap the entire block, else just memset it to 0 and set GET_ALLOC to 0?
+	// if memory infront of it isn't allocated, can just combine the two to make a bigger segment
 	munmap(addr, 5); // void*, size
+	void *ptrToHead = HDRP(addr);
+	GET_ALLOC(ptrToHead) = 0; 
 }
 
 void *realloc(void *ptr, size_t size)
@@ -10,32 +16,108 @@ void *realloc(void *ptr, size_t size)
 	return (NULL);
 }
 
-// void	add_to_list(void *ptr, int size)
-// {
-// 	(void)ptr;(void)size;
-// }
+void *find_spot(size_t size, int type)
+{
+	if (!((void**)&global)[type])
+		return (NULL);
+	void *ptr = ((void**)&global)[type];
+	while (GET_ALLOC(ptr) || GET_SIZE(ptr) < size + sizeof(void*)) // need to save room for a void * at the end
+		ptr += OVERHEAD + GET_SIZE(ptr);
+	if (GET_ALLOC(ptr) || GET_SIZE(ptr) >= size + sizeof(void*))
+	{
+		// need to do another mmap but need to link it to the end of the most recent mmap
+	}
+	return (ptr);
+}
+
+void	*init_memory(int size, int type)
+{
+	void *addr;
+
+	((void**)&global)[type] = mmap(0, size + OVERHEAD, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	addr = ((void**)&global)[type];
+	// set current head
+	block_header head;
+	head.size = size;
+	head.allocated = 1;
+	*(block_header*)addr = head;
+	// set next header at end of current header
+	block_header next_head;
+	next_head.size = PAGE_SIZE*(size/PAGE_SIZE+1) - OVERHEAD*2 - size;
+	next_head.allocated = 0;
+	*(block_header*)(addr + OVERHEAD + size) = next_head;
+	return (addr);
+}
 
 void *malloc(size_t size)
 {
-	void *addr = mmap(0, size,
-		PROT_READ | PROT_WRITE,
-		MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	void *addr;
+	int type;
+	if (size <= TINY)
+		type = 0;
+	else if (size <= SMALL)
+		type = 1;
+	else
+		type = 2;
+
+	if (!(addr = find_spot(size + OVERHEAD, type)))
+	{
+		addr = init_memory(size, type);
+	}
+	else // is spot in memory already allocated
+	{
+		int save = GET_SIZE(addr);
+		(*(block_header*)addr).size = size;
+		(*(block_header*)addr).allocated = 1;
+		// set next header at end of current header, need to change to check if there is room
+		block_header next_head;
+		next_head.size = save - OVERHEAD - size;
+		next_head.allocated = 0;
+		*(block_header*)(addr + OVERHEAD + size) = next_head;
+	}
 	if (addr == MAP_FAILED)
 		return (NULL);
-	// add_to_list(addr, size);
 
-	return (addr);
+	return (addr + OVERHEAD);
 }
 
 int		main(void)
 {
-	char *str = (char *)malloc(sizeof(char) * 5);
-	for (int i = 0; i < 4; i++)
-	{
-		str[i] = 'a' + i;
-	}
-	str[4] = '\0';
-	printf("Test %s\n", str);
-	printf("%lu\n", sizeof(block_header));
-	free(str);
+	void *ptr = malloc(sizeof(char) * 70);
+	void *ptr2 = malloc(sizeof(char) * 300);
+	void *ptr3 = malloc(sizeof(char) * 500);
+	void *ptr4 = malloc(sizeof(char) * 30);
+	void *ptr5 = malloc(sizeof(char) * 5000);
+
+	for (int i = 0; i < 5; i++)
+		((char*)ptr)[i] = 'a';
+	for (int i = 0; i < 5; i++)
+		((char*)ptr2)[i] = 'b';
+	for (int i = 0; i < 5; i++)
+		((char*)ptr3)[i] = 'c';
+	for (int i = 0; i < 5; i++)
+		((char*)ptr4)[i] = 'd';
+	for (int i = 0; i < 5000; i++)
+		((char*)ptr5)[i] = 'e';
+
+
+	printf("%lu, %d, %c\n", GET_SIZE(HDRP(ptr)), GET_ALLOC(HDRP(ptr)), ((char*)ptr)[0]);
+	free(ptr);
+	printf("%lu, %d, %c\n", GET_SIZE(HDRP(ptr)), GET_ALLOC(HDRP(ptr)), ((char*)ptr)[0]);
+
+	printf("%lu, %d, %c\n", GET_SIZE(HDRP(ptr2)), GET_ALLOC(HDRP(ptr2)), ((char*)ptr2)[0]);
+	free(ptr2);
+	printf("%lu, %d, %c, %lu\n", GET_SIZE(HDRP(ptr2)), GET_ALLOC(HDRP(ptr2)), ((char*)ptr2)[0], GET_SIZE(NEXT_BLKP(ptr2)));
+
+	printf("%lu, %d, %c\n", GET_SIZE(HDRP(ptr3)), GET_ALLOC(HDRP(ptr3)), ((char*)ptr3)[0]);
+	free(ptr3);
+	printf("%lu, %d, %c, %lu\n", GET_SIZE(HDRP(ptr3)), GET_ALLOC(HDRP(ptr3)), ((char*)ptr3)[0], GET_SIZE(NEXT_BLKP(ptr3)));
+
+	printf("%lu, %d, %c\n", GET_SIZE(HDRP(ptr4)), GET_ALLOC(HDRP(ptr4)), ((char*)ptr4)[0]);
+	free(ptr4);
+	printf("%lu, %d, %c, %lu\n", GET_SIZE(HDRP(ptr4)), GET_ALLOC(HDRP(ptr4)), ((char*)ptr4)[0], GET_SIZE(NEXT_BLKP(ptr4)));
+
+	printf("%lu, %d, %c\n", GET_SIZE(HDRP(ptr5)), GET_ALLOC(HDRP(ptr5)), ((char*)ptr5)[0]);
+	free(ptr5);
+	printf("%lu, %d, %c, %lu\n", GET_SIZE(HDRP(ptr5)), GET_ALLOC(HDRP(ptr5)), ((char*)ptr5)[0], GET_SIZE(NEXT_BLKP(ptr5)));
 }
